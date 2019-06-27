e42935dd331433ba1e7d53cd91e4c58b84d4d87e
Masaki Kuyama <masaki_kuyama@yahoo.co.jp>
2019-06-28 00:17:15 +0900

    SimpleFactoryパターンの修正

--- create/simple_factory/sample/main.php
+++ create/simple_factory/sample/main.php
@@ -1,6 +1,6 @@
 <?php
 include_once('DoorFactory.php');
 
-$door = FoodFactory::makeDoor(100, 200);
+$door = DoorFactory::makeDoor(100, 200);
 echo 'Width: ' . $door->getWidth();
 echo 'Height: ' . $door->getHeight();
